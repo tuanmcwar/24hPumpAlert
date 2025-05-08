1215ea5595b71d57e92668add3d6aac520e11b4d
tuanmcwar <tuanntch1@gmail.com>
2025-05-08 21:15:56 +0700

    newtime

--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 
 function startCronJob() {
     // Chạy lúc 06:45 sáng mỗi ngày
-    cron.schedule('25 21 * * *', () => {
+    cron.schedule('45 23 * * *', () => {
         console.log('⏰ [CRON] 06:45 sáng – đang chạy fetchAllData...');
         fetchAllData();
     });
